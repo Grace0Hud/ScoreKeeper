3cae171a8e6e33f317b3adf2a9ef418141ad0047
Grace0Hud <54286731+Grace0Hud@users.noreply.github.com>
2020-09-23 16:48:41 -0700

    made newGame button work from homescreen to newgame

--- app/src/main/java/com/example/scorekeeper/MainActivity.java
+++ app/src/main/java/com/example/scorekeeper/MainActivity.java
@@ -2,15 +2,22 @@ package com.example.scorekeeper;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
 
-
+    public void toNewGame(View view)
+    {
+        Intent intent = new Intent(this, NewGameScreen.class);
+        startActivity(intent);
+    }
 }
