c614073ce0f3656642088f00fe8861d6d01abf4f
Grace0Hud <54286731+Grace0Hud@users.noreply.github.com>
2020-09-23 16:11:49 -0700

    creation of new game screen

---
+++ app/src/main/java/com/example/scorekeeper/NewGameScreen.java
@@ -0,0 +1,14 @@
+package com.example.scorekeeper;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NewGameScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_game_screen);
+    }
+}
