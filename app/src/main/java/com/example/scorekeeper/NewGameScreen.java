5f3a9cb3c8277077e4ce966aef749086d0ce28a1
Grace0Hud <54286731+Grace0Hud@users.noreply.github.com>
2020-09-24 08:59:53 -0700

    Game screen creation - buttons/title/layout

--- app/src/main/java/com/example/scorekeeper/NewGameScreen.java
+++ app/src/main/java/com/example/scorekeeper/NewGameScreen.java
@@ -2,7 +2,9 @@ package com.example.scorekeeper;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class NewGameScreen extends AppCompatActivity {
 
@@ -10,5 +12,12 @@ public class NewGameScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_game_screen);
+
+    }
+
+    public void toGame(View view)
+    {
+        Intent intent = new Intent(this, gameScreen.class);
+        startActivity(intent);
     }
 }
