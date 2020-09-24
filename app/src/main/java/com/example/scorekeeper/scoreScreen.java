47db901d9ea1000544404b8d909b5144f1088461
Grace0Hud <54286731+Grace0Hud@users.noreply.github.com>
2020-09-24 14:38:33 -0700

    creation of score screen

---
+++ app/src/main/java/com/example/scorekeeper/scoreScreen.java
@@ -0,0 +1,14 @@
+package com.example.scorekeeper;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class scoreScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_score_screen2);
+    }
+}
