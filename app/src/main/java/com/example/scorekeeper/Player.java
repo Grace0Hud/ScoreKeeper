90b2b19fa51aadf4991d37773320d4d15a4953ea
Grace0Hud <54286731+Grace0Hud@users.noreply.github.com>
2020-09-25 15:06:46 -0700

    Player constructor

--- app/src/main/java/com/example/scorekeeper/Player.java
+++ app/src/main/java/com/example/scorekeeper/Player.java
@@ -1,4 +1,14 @@
 package com.example.scorekeeper;
 
-public class Player {
-}
+public class Player
+{
+    private String name;
+    private int score;
+
+    public Player(String name)
+    {
+        this.name = name;
+        this.score = 0;
+    }//end constructor
+
+}//end Player class
