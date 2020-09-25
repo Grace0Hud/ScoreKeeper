601d69c236792a8126a57138084e36dac36049a0
Grace0Hud <54286731+Grace0Hud@users.noreply.github.com>
2020-09-25 15:23:29 -0700

    Player getters and setters completed

--- app/src/main/java/com/example/scorekeeper/Player.java
+++ app/src/main/java/com/example/scorekeeper/Player.java
@@ -3,17 +3,20 @@ package com.example.scorekeeper;
 public class Player {
     private String name;
     private int score;
-
+    private static int numPlayers = 0;
     public Player(String name) {
         this.name = name;
         this.score = 0;
+        numPlayers++;
     }//end constructor
 
+    //method will add certain amount of points depending on button pressed
     public void addPoints(int add)
     {
         score = score + add;
     }//end addPoints method
 
+    //sets score back to 0
     public void resetScore()
     {
         score = 0;
@@ -24,4 +27,18 @@ public class Player {
         return score;
     }//end getScore
 
+    public void setPlayerName(String name)
+    {
+        this.name = name;
+    }//end setName
+
+    public String getPlayerName()
+    {
+        return name;
+    }//end get name
+
+    public void resetNumPlayers()
+    {
+        numPlayers = 0;
+    }//end resetNumPlayers
 }//end Player class
