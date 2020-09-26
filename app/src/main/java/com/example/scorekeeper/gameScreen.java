221c2a37d422540ead01ec772b4d2d9e4b6b396e
Grace0Hud <54286731+Grace0Hud@users.noreply.github.com>
2020-09-25 17:01:36 -0700

    retrival of information from NewGameScreen

--- app/src/main/java/com/example/scorekeeper/gameScreen.java
+++ app/src/main/java/com/example/scorekeeper/gameScreen.java
@@ -8,6 +8,14 @@ import android.view.View;
 
 public class gameScreen extends AppCompatActivity
 {
+    String player1Name = getIntent().getStringExtra("player1Name");
+    String player2Name = getIntent().getStringExtra("player2Name");
+    String player3Name = getIntent().getStringExtra("player3Name");
+    String player4Name = getIntent().getStringExtra("player4Name");
+    String player5Name = getIntent().getStringExtra("player5Name");
+    String player6Name = getIntent().getStringExtra("player6Name");
+    String player7Name = getIntent().getStringExtra("player7Name");
+    String player8Name = getIntent().getStringExtra("player8Name");
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -20,5 +28,5 @@ public class gameScreen extends AppCompatActivity
     {
         Intent intent = new Intent(this, winScreen.class);
         startActivity(intent);
-    }
-}
+    }//end onClickEndGame
+}//end gameScreen
