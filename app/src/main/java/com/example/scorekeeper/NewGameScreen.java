1222796809c9740c5f66bdb09b0adad7822fbea1
Grace0Hud <54286731+Grace0Hud@users.noreply.github.com>
2020-09-28 13:10:11 -0700

    well, now it doesn't crash when you go from newGameScreen to gameScreen

--- app/src/main/java/com/example/scorekeeper/NewGameScreen.java
+++ app/src/main/java/com/example/scorekeeper/NewGameScreen.java
@@ -9,39 +9,44 @@ import android.widget.EditText;
 
 public class NewGameScreen extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_game_screen);
 
+
     }
     public void toGame(View view)
     {
         //retreiving inormation from the edit views
         EditText player1Input = findViewById(R.id.player1Input);
-        String player1Name =  player1Input.getText().toString();
-
         EditText player2Input = findViewById(R.id.player2Input);
-        String player2Name =  player2Input.getText().toString();
-
         EditText player3Input = findViewById(R.id.player3Input);
-        String player3Name =  player3Input.getText().toString();
-
         EditText player4Input = findViewById(R.id.player4Input);
-        String player4Name =  player4Input.getText().toString();
-
         EditText player5Input = findViewById(R.id.player5Input);
-        String player5Name =  player5Input.getText().toString();
-
         EditText player6Input = findViewById(R.id.player6Input);
-        String player6Name =  player6Input.getText().toString();
-
         EditText player7Input = findViewById(R.id.player7Input);
-        String player7Name =  player7Input.getText().toString();
-
         EditText player8Input = findViewById(R.id.player8Input);
-        String player8Name =  player8Input.getText().toString();
+
+        String player1Name = "";
+        String player2Name = "";
+        String player3Name = "";
+        String player4Name = "";
+        String player5Name = "";
+        String player6Name = "";
+        String player7Name = "";
+        String player8Name = "";
+
+        player1Name = player1Input.getText().toString();
+        player2Name = player2Input.getText().toString();
+        player3Name = player3Input.getText().toString();
+        player4Name = player4Input.getText().toString();
+        player5Name = player5Input.getText().toString();
+        player6Name = player6Input.getText().toString();
+        player7Name = player7Input.getText().toString();
+        player8Name = player8Input.getText().toString();
 
         //to game screen
         Intent intent = new Intent(this, gameScreen.class);
--- app/src/main/java/com/example/scorekeeper/gameScreen.java
+++ app/src/main/java/com/example/scorekeeper/gameScreen.java
@@ -5,23 +5,51 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class gameScreen extends AppCompatActivity
 {
-    String player1Name = getIntent().getStringExtra("player1Name");
-    String player2Name = getIntent().getStringExtra("player2Name");
-    String player3Name = getIntent().getStringExtra("player3Name");
-    String player4Name = getIntent().getStringExtra("player4Name");
-    String player5Name = getIntent().getStringExtra("player5Name");
-    String player6Name = getIntent().getStringExtra("player6Name");
-    String player7Name = getIntent().getStringExtra("player7Name");
-    String player8Name = getIntent().getStringExtra("player8Name");
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_screen);
+        String player1Name = getIntent().getStringExtra("player1Name");
+        String player2Name = getIntent().getStringExtra("player2Name");
+        String player3Name = getIntent().getStringExtra("player3Name");
+        String player4Name = getIntent().getStringExtra("player4Name");
+        String player5Name = getIntent().getStringExtra("player5Name");
+        String player6Name = getIntent().getStringExtra("player6Name");
+        String player7Name = getIntent().getStringExtra("player7Name");
+        String player8Name = getIntent().getStringExtra("player8Name");
+
+        Player player1 = new Player(player1Name);
+        Player player2 = new Player(player2Name);
+        Player player3 = new Player(player3Name);
+        Player player4 = new Player(player4Name);
+        Player player5 = new Player(player5Name);
+        Player player6 = new Player(player6Name);
+        Player player7 = new Player(player7Name);
+        Player player8 = new Player(player8Name);
+
+        Button player1Button = findViewById(R.id.player1Display);
+        player1Button.setText(player1.getPlayerName() + ": " + player1.getScore());
+        Button player2Button = findViewById(R.id.player2Display);
+        player2Button.setText(player2.getPlayerName() + ": " + player2.getScore());
+        Button player3Button = findViewById(R.id.player3Display);
+        player3Button.setText(player3.getPlayerName() + ": " + player3.getScore());
+        Button player4Button = findViewById(R.id.player4Display);
+        player4Button.setText(player4.getPlayerName() + ": " + player4.getScore());
+        Button player5Button = findViewById(R.id.player5Display);
+        player5Button.setText(player5.getPlayerName() + ": " + player5.getScore());
+        Button player6Button = findViewById(R.id.player6Display);
+        player6Button.setText(player6.getPlayerName() + ": " + player6.getScore());
+        Button player7Button = findViewById(R.id.player7Display);
+        player7Button.setText(player7.getPlayerName() + ": " + player7.getScore());
+        Button player8Button = findViewById(R.id.player8Display);
+        player8Button.setText(player8.getPlayerName() + ": " + player8.getScore());
     }
 
     public void onClickEndGame(View view)
