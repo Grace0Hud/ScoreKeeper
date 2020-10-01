629bacd16ee425b5a189564981b5c18eb6b8df63
Grace0Hud <54286731+Grace0Hud@users.noreply.github.com>
2020-09-30 18:54:22 -0700

    display winner name if won by winning

--- app/src/main/java/com/example/scorekeeper/winScreen.java
+++ app/src/main/java/com/example/scorekeeper/winScreen.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class winScreen extends AppCompatActivity {
 
@@ -12,6 +13,12 @@ public class winScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_win_screen);
+
+        //displaying the name of the winning player
+        String winner = getIntent().getStringExtra("winner");
+        TextView winnerDisplay = findViewById(R.id.winnerDisplay);
+
+        winnerDisplay.setText(winner + " Wins");
     }
 
     public void onNewGameClick(View view)
