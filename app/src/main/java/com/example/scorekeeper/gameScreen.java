3a4a220a6e1c63da5d771e0269111e7556cc584f
Grace0Hud <54286731+Grace0Hud@users.noreply.github.com>
2020-10-12 10:25:27 -0700

    can add new players on Game Screen

--- app/src/main/java/com/example/scorekeeper/gameScreen.java
+++ app/src/main/java/com/example/scorekeeper/gameScreen.java
@@ -1,12 +1,16 @@
 package com.example.scorekeeper;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -233,6 +237,45 @@ public class gameScreen extends AppCompatActivity
         //starts up win screen
         startActivity(intent);
     }//end onClickEndGame
+
+    public void onClickAddPlayer(View view) {
+        // get prompts.xml view
+        LayoutInflater layoutInflater = LayoutInflater.from(this);
+
+        View promptView = layoutInflater.inflate(R.layout.add_player_dialogue, null);
+
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+
+        // set prompts.xml to be the layout file of the alertdialog builder
+        alertDialogBuilder.setView(promptView);
+
+        final EditText input = (EditText) promptView.findViewById(R.id.newPlayerName);
+
+        // setup a dialog window
+        alertDialogBuilder
+                .setCancelable(false)
+                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        // get user input and set it to result
+                        // editTextMainScreen.setText(input.getText());
+                        players.add(new Player(input.getText().toString()));
+                        playerButtons.get(players.size()-1).setText(players.get(players.size()-1).getPlayerName() + ": " + players.get(players.size()-1).getScore());
+                        playerButtons.get(players.size()-1).setVisibility(View.VISIBLE);
+
+                    }
+                })
+                .setNegativeButton("Cancel",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                dialog.cancel();
+                            }
+                        });
+
+        // create an alert dialog
+        AlertDialog alertD = alertDialogBuilder.create();
+
+        alertD.show();
+}
 
     //when player buttons are clicked
     public void onClickPlayer1Button(View view)
